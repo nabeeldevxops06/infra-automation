e27559060c6525eb96700d6b51f5cc70418bdc06
Muhmmad Nabeel <nabeel@Muhmmads-MacBook-Pro.local>
2025-05-22 21:12:30 +0500

    feat: add Azure Terraform module

---
+++ azure/main.tf
@@ -0,0 +1,32 @@
+# terraform {
+#   required_version = ">= 1.5.0"
+#   backend "azurerm" {
+#     resource_group_name  = "infra-auto-rg"
+#     storage_account_name = "infraautostate${random_id.hex}"
+#     container_name       = "tfstate"
+#     key                  = "azure/terraform.tfstate"
+#     subscription_id      = "YOUR_SUB_ID"
+#     tenant_id            = "YOUR_TENANT_ID"
+#     encrypt              = true
+#   }
+# }
+
+# provider "azurerm" {
+#   features {}
+# }
+
+# resource "azurerm_resource_group" "rg" {
+#   name     = "infra-auto-rg"
+#   location = "East US"
+# }
+
+# resource "azurerm_storage_account" "state" {
+#   name                     = "infraautostate${random_id.hex}"
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
+
+# resource "random_id" "hex" {
+#   byte_length = 4
+# }
