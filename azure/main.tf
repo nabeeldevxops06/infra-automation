699cc4d52800e1ad4cfb5c47be1c06247f4e44aa
Muhmmad Nabeel <nabeel@Muhmmads-MacBook-Pro.local>
2025-05-27 17:56:58 +0500

    Creating the Azure Blob Storage

--- azure/main.tf
+++ azure/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "state" {
-  name                     = "azstorage"
+  name                     = "azstorage${random_id.suffix.hex}"
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
